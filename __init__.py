c316339ac4c8cbcbc79aae0ba5ef57435af225e6
Youri Matthys <2657774+miruoy@users.noreply.github.com>
2026-09-12 13:53:29 +0200

    Update author email

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ reload(plugin)  # In case we're being reloaded.
 
 __version__ = '1.0.0'
 __author__ = supybot.Author(name='Youri Matthys', nick='miruoy',
-                             email='miruoy@users.noreply.github.com')
+                             email='yourimatthys+github@gmail.com')
 __contributors__ = {}
 __url__ = 'https://github.com/miruoy/limnoria-Replace'
 
